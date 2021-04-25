7d135c1502be1959fa7bff696607652b31e33c32
danisz118 <daniz118@gmail.com>
2021-04-25 12:13:00 +0300

    initial commit

---
+++ src/javaProfessional/simple/interrupt/Main.java
@@ -0,0 +1,33 @@
+package javaProfessional.simple.interrupt;
+
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        counter.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        counter.interrupt();
+    }
+}
+
+class Counter extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+
+        while (!isInterrupted()) {
+            System.out.println("i = " + i++);
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
